1d07950b53ee2cd8171852dcfc44c4af134a477a
VideoGamePlotliner <92762795+VideoGamePlotliner@users.noreply.github.com>
2021-10-18 20:06:28 -0400

    Importing the Java module 'vgp.base'

---
+++ vgp.base/src/module-info.java
@@ -0,0 +1,43 @@
+/**
+ * Package summary.
+ * <p>
+ * Package contents.
+ * <p>
+ * Package purpose.
+ * <p>
+ * Package specification.
+ * <p>
+ * Related documentation.
+ * 
+ * @author John Bues
+ * @version version
+ * @see reference
+ * @since version
+ */
+/**
+ * Module summary.
+ * <p>
+ * Module contents.
+ * <p>
+ * Module purpose.
+ * <p>
+ * Module specification.
+ * <p>
+ * Related documentation.
+ * <p>
+ * <strong>Change Log</strong>
+ * <ol>
+ * <li>Version ________________
+ *     <ul>
+ *     <li>Module {@code vgp.base} created
+ *     <li>Package ________________ created
+ *     <li>Class ________________ created
+ *     </ul>
+ * </ol>
+ * 
+ * @author John Bues
+ * @see reference
+ * @since version
+ */
+module vgp.base {
+}
