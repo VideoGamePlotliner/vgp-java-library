bbb5cc6f0992effeaa76df517bcabef26a8ab7dc
VideoGamePlotliner <92762795+VideoGamePlotliner@users.noreply.github.com>
2021-10-18 20:14:39 -0400

    Update module-info.java
    An import from an older version of this library.

--- vgp.base/src/module-info.java
+++ vgp.base/src/module-info.java
@@ -1,43 +1,56 @@
 /**
- * Package summary.
+ * Container for various "base" classes and interfaces, suitable for
+ * extension and implementation by future modules' subclasses.
  * <p>
- * Package contents.
+ * Similar to the module {@code vgp.fx.base}, this module is designed to
+ * contain superclasses and interfaces, not subclasses or implementing
+ * classes. As such, this module cannot {@code require} any other
+ * non-library modules. In other words, while this module can rely upon
+ * {@code java.base} (which it does by default) or another built-in
+ * module such as {@code java.xml}, this module cannot rely upon, say,
+ * {@code vgp.fx.base}.
  * <p>
- * Package purpose.
+ * In addition, this module should not rely on JUnit library modules.
+ * Normally, a desire to include JUnit test classes results in relying
+ * on JUnit library modules. However, <em>this</em> module should not
+ * contain JUnit tests for its <em>own</em> "base" classes, mainly
+ * because base classes are skeletons and thus do not implement much,
+ * if any, of the functionality they specify, meaning that whoever writes
+ * the source code for the test classes has to devise his/her own
+ * implementation. In other words, because most classes in this module
+ * are either abstract classes, interfaces, or skeleton concrete classes,
+ * <em>designers of tests for this module's classes should first
+ * implement said classes and then test the implementing classes.</em>
  * <p>
- * Package specification.
+ * Generally, this module's classes either define new functionality or
+ * redefine existing functionality. For example, this module's {@code
+ * ObjectBase} class defines new functionality by including fields and
+ * methods (excluding the getters and setters) that its superclass
+ * {@code Object} does <em>not</em> have. However, {@code ObjectBase}
+ * also redefines existing functionality by overriding methods that its
+ * superclass {@code Object} does <em>indeed</em> have.
  * <p>
- * Related documentation.
- * 
- * @author John Bues
- * @version version
- * @see reference
- * @since version
- */
-/**
- * Module summary.
- * <p>
- * Module contents.
- * <p>
- * Module purpose.
- * <p>
- * Module specification.
- * <p>
- * Related documentation.
+ * Theoretically, other modules, such as {@code vgp.fx.base},
+ * may structure themselves like this one by imposing the above
+ * specifications on <em>their</em> classes. In this case, an
+ * extender of the JavaFX {@code Pane} class (found in the module
+ * {@code javafx.graphics}) may redefine existing functionality not
+ * <em>only</em> by overriding superclass methods but <em>also</em>
+ * by customizing the superclass's properties via its setter methods.
+ * Please refer to the documentation for {@code vgp.fx.base} for
+ * examples and clarification.
  * <p>
  * <strong>Change Log</strong>
  * <ol>
- * <li>Version ________________
+ * <li>Version 1.0
  *     <ul>
  *     <li>Module {@code vgp.base} created
- *     <li>Package ________________ created
- *     <li>Class ________________ created
  *     </ul>
  * </ol>
  * 
  * @author John Bues
- * @see reference
- * @since version
+ * @see "The library module java.base"
+ * @see "The custom module vgp.fx.base"
  */
 module vgp.base {
 }
