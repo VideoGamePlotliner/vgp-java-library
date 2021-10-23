9e00a0722884eb684a571f62dae2666c57a272be
VideoGamePlotliner <92762795+VideoGamePlotliner@users.noreply.github.com>
2021-10-22 20:30:44 -0400

    Added ObjectBase.
    Adjusted the version number in package-info.java and the change log in module-info.java to reflect this addition.

--- vgp.base/src/vgp/sample/package-info.java
+++ vgp.base/src/vgp/sample/package-info.java
@@ -9,11 +9,12 @@
  * Currently, there are no plans to include interfaces or any other
  * classes in this package, but that could change in the future.
  * <p>
- * Lastly, for the sake of understanding this package's module better,
- * said module should {@code export} this package to all other modules.
+ * Lastly, for the sake of using {@code Object} to understand this
+ * package's module better, said module should {@code export} this
+ * package to all other modules.
  * 
  * @author John Bues
- * @version 1.1
+ * @version 1.2
  * @see java.lang
  * @since 1.1
  */
