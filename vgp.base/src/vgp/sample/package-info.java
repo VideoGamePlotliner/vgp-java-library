fa53264f7baa6c7980388527d74badde4e5b73a7
VideoGamePlotliner <92762795+VideoGamePlotliner@users.noreply.github.com>
2021-10-22 20:24:34 -0400

    Create package-info.java

---
+++ vgp.base/src/vgp/sample/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Container for a general base class that directly extends {@code
+ * Object}.
+ * <p>
+ * Said class, which is called {@code ObjectBase}, is designed to
+ * contain templates for {@code public}, overridable methods of the
+ * {@code Object} class.
+ * <p>
+ * Currently, there are no plans to include interfaces or any other
+ * classes in this package, but that could change in the future.
+ * <p>
+ * Lastly, for the sake of understanding this package's module better,
+ * said module should {@code export} this package to all other modules.
+ * 
+ * @author John Bues
+ * @version 1.1
+ * @see java.lang
+ * @since 1.1
+ */
+package vgp.sample;
